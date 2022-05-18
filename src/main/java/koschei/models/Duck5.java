d1eac7d17fbdf4bf14f3e846e2ac9744b056f636
kuzhilin <kuzhilin94@yandex.ru>
2022-05-18 09:51:30 +0700

    add beans

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,18 @@
 package koschei.models;
 
-import org.springframework.stereotype.Component;
 
-@Component
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Duck5 {
+    private Egg6 egg;
+
+    @Autowired
+    public Duck5(Egg6 egg) {
+        this.egg = egg;
+    }
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
     }
 }
